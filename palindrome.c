b8286e2d2f3f4f83839bae9e82672cbf3683a16e
Karisni <karisnidevibalan7@gmail.com>
2026-01-22 22:03:03 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,15 @@
+bool isPalindrome(int x) {
+    int rev=0;
+
+    if(x<0 || x%10==0 && x!=0){
+        return false;
+    }
+    while(x>rev){
+        rev=rev*10+x%10;
+        x=x/10;
+    }
+    if (rev==x || x==rev/10){
+        return true;
+    }
+    return false;
+}

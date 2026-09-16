b9f624da81441013f2bcbfd8545d83fdb402d371
Karisni <karisnidevibalan7@gmail.com>
2026-09-16 11:30:17 +0530

    Time: 23 ms (92.55%) | Memory: 103.7 MB (84.31%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,21 +1,13 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         Deque<Integer>s=new ArrayDeque<>();
-        Map<Integer,Integer>m=new HashMap<>();
         int[] arr=new int[temperatures.length];
         for(int i=0;i<temperatures.length;i++){
             while(!s.isEmpty() && temperatures[i]>temperatures[s.peek()]){
-                m.put(s.pop(),i);
+                int prev=s.pop();
+                arr[prev]=i-prev;
             }
             s.push(i);
-        }
-        for(int i=0;i<temperatures.length;i++){
-            if(m.containsKey(i)){
-            arr[i]=(m.get(i))-i;
-        }
-        else{
-            arr[i]=0;
-        }
         }
         return arr;
     }
